7c373dc60ba2eb62a5540f47a727a8a221daf84a
Ramy1951 <ramy1951@live.com>
2020-04-29 21:06:25 -0700

    Added the broadcasting function on recieving of message

---
+++ backend/src/main/java/demo/MessageDto.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class MessageDto {
+    public final String user;
+    public final String message;
+
+    public MessageDto(String user, String message) {
+        this.user = user;
+        this.message = message;
+    }
+}
--- backend/src/main/java/demo/WebSocketHandler.java
+++ backend/src/main/java/demo/WebSocketHandler.java
@@ -1,5 +1,6 @@
 package demo;
 
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.*;
 import java.io.*;
@@ -10,21 +11,38 @@ import java.util.concurrent.*;
 public class WebSocketHandler {
   // Store sessions if you want to, for example, broadcast a message to all users
   static Map<Session, Session> sessionMap = new ConcurrentHashMap<>();
+  Gson gson = new Gson();
+
+
+  public void broadcast(String message){
+    sessionMap.keySet().forEach( (session) ->{
+
+      MessageDto messagedto = new MessageDto("FILL_IN_USERNAME", message);
+
+      try{
+        session.getRemote().sendString(gson.toJson(messagedto, MessageDto.class));
+      }
+      catch (Exception e){
+        e.printStackTrace();
+      }
+    });
+  }
 
   @OnWebSocketConnect
   public void connected(Session session) throws IOException {
-    System.out.println("A client has connected");
+    session.getRemote().sendString("Connected");
     sessionMap.put(session, session);
   }
 
   @OnWebSocketClose
-  public void closed(Session session, int statusCode, String reason) {
-    System.out.println("A client has disconnected");
+  public void closed(Session session, int statusCode, String reason) throws IOException {
+    session.getRemote().sendString("Disconnected");
     sessionMap.remove(session);
   }
 
   @OnWebSocketMessage
   public void message(Session session, String message) throws IOException {
     System.out.println("Got: " + message);   // Print message
+    broadcast(message);
   }
 }
