67b950213ce7649f7303cd5f8ea8ceda3da89975
xo28122000 <jainam.2000@yahoo.com>
2020-04-30 21:55:22 -0700

    Home page basic layout

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,8 +4,15 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
+import SendIcon from "@material-ui/icons/Send";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import TextField from "@material-ui/core/TextField";
+
 // My imports
 import { logout } from "../redux/actions/index";
+import "../styles/Home.css";
 
 const Home = props => {
   const [totalUsers, setTotalUsers] = React.useState(0);
@@ -20,9 +27,40 @@ const Home = props => {
   React.useEffect(() => {}, []);
 
   return (
-    <div className="App">
-      {!props.isUser ? <Redirect to="/login" /> : null}
-      <button onClick={() => props.logout()}>LOGOUT</button>
+    <div id="Home-root">
+      {/* {!props.isUser ? <Redirect to="/login" /> : null} */}
+      <div id="Home-header">
+        <Button
+          className="Home-header-button"
+          variant="contained"
+          color="primary"
+          size="large"
+          onClick={() => props.logout()}
+        >
+          LOGOUT
+        </Button>
+        <Button
+          className="Home-header-button"
+          variant="contained"
+          color="primary"
+          size="large"
+          startIcon={<AccountCircleIcon />}
+          onClick={() => props.logout()}
+        >
+          Profile
+        </Button>
+      </div>
+      <div id="Home-footer">
+        <TextField
+          style={{ width: "80%" }}
+          label=""
+          defaultValue=""
+          variant="outlined"
+        /> 
+        <IconButton aria-label="delete">
+          <SendIcon fontSize="large" />
+        </IconButton>
+      </div>
     </div>
   );
 };
