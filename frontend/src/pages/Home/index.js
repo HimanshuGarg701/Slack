17572b791a8a4ca0ac323b84164b6ee1264dd367
xo28122000 <jainam.2000@yahoo.com>
2020-05-03 00:00:18 -0700

    drawer added

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,5 +1,5 @@
 // 3rd party imports
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
@@ -10,26 +10,77 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import TextField from "@material-ui/core/TextField";
 
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+
 // My imports
 import { logout } from "../../redux/actions/index";
 import Message from "../../components/Message/index";
 import "./styles.css";
+import Axios from "axios";
+
+const ws = new WebSocket("ws://localhost:1235/ws");
 
 const Home = props => {
-  const [totalUsers, setTotalUsers] = React.useState(0);
-  const [notes, setNotes] = React.useState(["test"]);
-  const [note, setNote] = React.useState("");
+  // const [totalUsers, setTotalUsers] = React.useState(0);
+  const [msgObjs, setMsgObjs] = React.useState([
+    { body: "heyy", username: "jainamshah", id: "1", likes: 2 },
+    { body: "yo", username: "blahha", id: "2", likes: 2 },
+    { body: "whatsup", username: "blahha", id: "3", likes: 2 },
+    { body: "Im playing PUBG!!!", username: "jainamshah", id: "4", likes: 2 }
+  ]);
 
-  const handleSubmit = () => {
-    console.log(note);
-    setNote("");
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const addMsgOnj = msgObj => {
+    setMsgObjs(msgObjs.concat([msgObj]));
   };
 
-  React.useEffect(() => {}, []);
+  useEffect(() => {
+    // request backend for initial messages
+    Axios.get("/getMessages").then(res => {
+      // success, payload;
+
+      if (res.success) {
+        setMsgObjs(res.payload);
+      } else {
+      }
+    });
+    // make a socket connection to add notes as we go on
+    // msgObj - {body-string, username-string,id-id of the message}
+    ws.addEventListener("message", async msgObj => {
+      addMsgOnj(msgObj);
+    });
+  }, []);
+
+  const sendMessage = () => {
+    let msgBody = document.getElementById("message-input").value;
+    if (msgBody.length > 0) {
+      // send a socket notif to bakend
+      // {body-string, id-idOFtheuser}
+      ws.send({ id: props.userObj.id, body: msgBody });
+    }
+  };
 
   return (
     <div id="Home-root">
       {/* {!props.isUser ? <Redirect to="/login" /> : null} */}
+
+      <SwipeableDrawer
+        anchor={"right"}
+        open={drawerOpen}
+        onClose={() => {
+          setDrawerOpen(false);
+        }}
+      >
+        hello
+      </SwipeableDrawer>
+
       <div id="Home-header">
         <Button
           className="Home-header-button"
@@ -40,38 +91,35 @@ const Home = props => {
         >
           LOGOUT
         </Button>
-        <Button
-          className="Home-header-button"
-          variant="contained"
-          color="primary"
-          size="large"
-          startIcon={<AccountCircleIcon />}
-          onClick={() => props.logout()}
+
+        <IconButton
+          aria-label="users"
+          onClick={() => setDrawerOpen(!drawerOpen)}
         >
-          Profile
-        </Button>
+          <AccountCircleIcon color="primary" style={{ fontSize: 45 }} />
+        </IconButton>
       </div>
       <div id="Home-body">
-        {[1, 2, 3].map(num => {
-          return <Message body={num} self={false} />;
+        {msgObjs.map(msgObj => {
+          return (
+            <Message body={msgObj.body} name={msgObj.username} self={true} />
+          );
         })}
-        <Message
-          body={
-            "JainamShahJainamShahJainamShahJainamShahJainamShahJainamShahJainamShahJainamShahJainamShahJainamShahJainamShahJainamShahJainamShahJainam Shah"
-          }
-          name={"Jainam"}
-          self={true}
-        />
-        <Message body={"ahJainamhJainam Shah"} self={true} />
       </div>
       <div id="Home-footer">
         <TextField
+          id="message-input"
           style={{ width: "80%" }}
           label=""
           defaultValue=""
           variant="outlined"
         />
-        <IconButton aria-label="delete">
+        <IconButton
+          aria-label="send"
+          onClick={() => {
+            sendMessage();
+          }}
+        >
           <SendIcon fontSize="large" />
         </IconButton>
       </div>
