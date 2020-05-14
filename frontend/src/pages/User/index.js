d1870183e63e49bf4af6db1a17a24dbe007d39d4
Mjcanson <mcanson@mail.sfsu.edu>
2020-05-13 17:41:34 -0700

    fixing up update password

--- frontend/src/pages/User/index.js
+++ frontend/src/pages/User/index.js
@@ -23,44 +23,72 @@ import './styles.css'
 import axios from 'axios'
 
 const User = () => {
-    const [username, setUsername] = useState('Mjcanson');
-    const [password, SetPassword] = useState('');
-    const [newPassword, SetNewPassword] = useState('');
-    const [error,setError] = useState(null);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [oldPassword, setOldPassword] = useState('');
+    const [verifyNewPassword, setVerifyNewPassword] = useState('');
+
+    const [newPassword, setNewPassword] = useState('');
+    const [passwordError, setPasswordError] = useState(false);
+    const [newPasswordError, setNewPasswordError] = useState(false);
+
+
+    const [error, setError] = useState(null);
 
     const [showMenu, setShowMenu] = useState(false);
     const [anchorEl, setAnchorEl] = useState(null);
     const [showChangePassword, setShowChangePassword] = useState(false);
+    console.log(newPassword)
+    console.log(verifyNewPassword)
+    console.log(oldPassword)
 
     const handleChangePassword = () => {
-        const body = {
+        // handleVerifyPassword();
 
-            username: username,
-            password: password,
-            newPassword: newPassword
+      
+            const body = {
 
-        }
+                username: username,
+                password: oldPassword,
+                newPassword: newPassword
 
-        axios.post('/auth/updateUser',body)
-        .then((res) => {
-            
-            if(res.data.success) {
-                console.log('password changed succesfully!');
-                console.log(res.data.password);
-
-            }else {
-                setError(res.data.error);
             }
-        })
-        .catch(()=> {
-            setError('password change failed. Try again.');
-        });
-    }
 
+            axios.post('/auth/updateUser', body)
+                
+                .then((res) => {
+                    console.log(res)
+                    if (res.data.success) {
+                        console.log(res.data.BaseResponse)
+                        console.log('password changed succesfully!');
+                        console.log(res.data.password);
+                        handleshowChangePasswordClose()
+
+                    } else {
+                        setError(res.data.error);
+                    }
+                })
+                .catch(() => {
+                    setError('password change failed. Try again.');
+                });
+        console.log(error)
+    }
 
     const handleshowChangePassword = e => {
         setShowChangePassword(true);
+
     };
+    const handleVerifyPassword = () => {
+        const newPass = newPassword.localeCompare(verifyNewPassword);
+        if (newPass === 0) {
+            console.log('new password matches!')
+
+        } else {
+            console.log('new password does not match')
+            setNewPasswordError(true);
+        }
+
+    }
 
     const handleClick = e => {
         setAnchorEl(e.currentTarget);
@@ -73,18 +101,18 @@ const User = () => {
     const handleClose = () => {
         setShowMenu(null);
     };
-   
+
 
     const handleshowChangePasswordClose = () => {
         setShowChangePassword(false);
     };
-    const HomeIcon =(props)=> {
+    const HomeIcon = (props) => {
         return (
-          <SvgIcon {...props}>
-            <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-          </SvgIcon>
+            <SvgIcon {...props}>
+                <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+            </SvgIcon>
         );
-      }
+    }
 
 
     const open = Boolean(anchorEl);
@@ -103,14 +131,14 @@ const User = () => {
                         <Typography variant="h6" className="header-title">
                             {username}'s Profile
                         </Typography>
-                        <HomeIcon fontSize="large"  />
+                        <HomeIcon fontSize="large" />
                         <Button color="inherit">Logout</Button>
                     </Toolbar>
                 </AppBar>
             </div>
 
             <div className="paper-root">
-                <Paper color="blue" classname="profile-paper" elevation={5} >
+                <Paper color="blue" elevation={5} >
                     <Grid container spacing={2}
                         direction="column"
                         alignItems="center"
@@ -120,7 +148,7 @@ const User = () => {
 
                             <Typography variant="h6" className="header-title">
                                 @{username}
-                                
+
                             </Typography>
 
                         </Grid>
@@ -139,7 +167,7 @@ const User = () => {
                                 keepMounted
                                 open={Boolean(anchorEl)}
                                 onClose={handlePopperClose}>
-                                
+
                                 <MenuItem onClick={() => {
                                     handleClose();
                                     handleshowChangePassword();
@@ -155,29 +183,34 @@ const User = () => {
                                 <DialogContentText>
                                     Please verify your old password before entering new password.
                                     </DialogContentText>
-                                    <TextField
-                                    // error= " "
+                                <TextField
+                                    onChange={(e) => { setOldPassword(e.target.value)}}
+                                    error={passwordError}
                                     autoFocus
                                     margin="dense"
                                     id="name"
                                     label="Enter old Password"
-                                    type="text"
+                                    type="password"
                                     fullWidth
                                 />
                                 <TextField
+                                    onChange={(e) => { setNewPassword(e.target.value) }}
+                                    error={newPasswordError}
                                     autoFocus
                                     margin="dense"
                                     id="name"
                                     label="new Password"
-                                    type="text"
+                                    type="password"
                                     fullWidth
                                 />
-                                     <TextField
+                                <TextField
+                                    onChange={(e) => { setVerifyNewPassword(e.target.value) }}
+                                    error={newPasswordError}
                                     autoFocus
                                     margin="dense"
                                     id="name"
                                     label="verify new Password"
-                                    type="text"
+                                    type="password"
                                     fullWidth
                                 />
                             </DialogContent>
@@ -185,16 +218,12 @@ const User = () => {
                                 <Button onClick={handleshowChangePasswordClose} color="primary">
                                     Cancel
                                 </Button>
-                                <Button onClick={()=> {
-                                    handleChangePassword();
-                                    if(error=== null)
-                                        handleshowChangePasswordClose();
-                                    }} color="primary">
+                                <Button onClick={handleChangePassword} color="primary">
                                     Confirm
                                 </Button>
                             </DialogActions>
                         </Dialog>
-                       
+
                     </Grid>
                 </Paper>
             </div>
