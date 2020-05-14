35f71992051bc166d4aae5ba9725abf923d6700f
Steven McHenry <stevenmchenry01@gmail.com>
2020-05-13 17:45:29 -0700

    added theming

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,112 +1,109 @@
 // 3rd party imports
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from 'react'
 
-import { Redirect } from "react-router-dom";
-import { connect } from "react-redux";
+import { Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-import SendIcon from "@material-ui/icons/Send";
-import AccountCircleIcon from "@material-ui/icons/AccountCircle";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import TextField from "@material-ui/core/TextField";
-
-import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
-import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
+import SendIcon from '@material-ui/icons/Send'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+import SettingsBrightnessIcon from '@material-ui/icons/SettingsBrightness'
+import {
+  Button,
+  IconButton,
+  TextField,
+  SwipeableDrawer,
+  useTheme,
+} from '@material-ui/core'
 
 // My imports
-import { logout } from "../../redux/actions/index";
-import Message from "../../components/Message/index";
-import "./styles.css";
-import Axios from "axios";
-import CurrentActiveUsers from "../../components/CurrentActiveUsers";
+import { logout } from '../../redux/actions/index'
+import Message from '../../components/Message/index'
+import './styles.css'
+import Axios from 'axios'
+import CurrentActiveUsers from '../../components/CurrentActiveUsers'
 
-const ws = new WebSocket("ws://localhost:1235/ws");
+const ws = new WebSocket('ws://localhost:1235/ws')
 
-const Home = props => {
+const Home = (props) => {
+  const theme = useTheme()
   // const [totalUsers, setTotalUsers] = React.useState(0);
-  const [msgObjs, setMsgObjs] = React.useState([]);
+  const [msgObjs, setMsgObjs] = React.useState([])
 
-  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [drawerOpen, setDrawerOpen] = useState(false)
 
-  const addMsgObj = msgObj => {
-    msgObjs.push(msgObj);
-    setMsgObjs([...msgObjs]);
+  const addMsgObj = (msgObj) => {
+    msgObjs.push(msgObj)
+    setMsgObjs([...msgObjs])
     // console.log(msgObjs);
-  };
+  }
 
   useEffect(() => {
     // request backend for initial messages
-    Axios.get("/getMessages").then(res => {
+    Axios.get('/getMessages').then((res) => {
       // success, payload;
       if (res.data.success) {
         for (let i = 0; i < res.data.payload.length; i++) {
-          addMsgObj(res.data.payload[i]);
+          addMsgObj(res.data.payload[i])
         }
       } else {
-        alert("Please reload the page");
+        alert('Please reload the page')
       }
-    });
+    })
     // make a socket connection to add notes as we go on
 
-    ws.addEventListener("message", async msgObj => {
-      let parsedData = JSON.parse(msgObj.data); 
-      if (parsedData.typeOfMessage == "MESSAGE") {
+    ws.addEventListener('message', async (msgObj) => {
+      let parsedData = JSON.parse(msgObj.data)
+      if (parsedData.typeOfMessage == 'MESSAGE') {
         //
-        addMsgObj(parsedData.payload);
+        addMsgObj(parsedData.payload)
         //
-      } else if (parsedData.typeOfMessage == "LIKE") {
+      } else if (parsedData.typeOfMessage == 'LIKE') {
         //
         for (let i = 0; i < msgObjs.length; i++) {
           if (parsedData.payload.id == msgObjs[i].id) {
             // change that object to this new object
-            msgObjs[i].likes = parsedData.payload.likes;
-            msgObjs[i].likeCount = parsedData.payload.likeCount;
-            setMsgObjs([...msgObjs]);
-            break;
+            msgObjs[i].likes = parsedData.payload.likes
+            msgObjs[i].likeCount = parsedData.payload.likeCount
+            setMsgObjs([...msgObjs])
+            break
           }
         }
         //
       }
-    });
-  }, []);
+    })
+  }, [])
 
   useEffect(() => {
-    var messageContainer = document.getElementById("Home-body");
-    messageContainer.scrollTop = messageContainer.scrollHeight;
-  }, [msgObjs.length]);
+    var messageContainer = document.getElementById('Home-body')
+    messageContainer.scrollTop = messageContainer.scrollHeight
+  }, [msgObjs.length])
 
   const sendMessage = () => {
-    let msgBody = document.getElementById("message-input");
+    let msgBody = document.getElementById('message-input')
     if (msgBody.value.length > 0) {
       ws.send(
         JSON.stringify({
           username: props.userObj.username,
-          body: msgBody.value
+          body: msgBody.value,
         })
-      );
+      )
 
-      msgBody.value = "";
+      msgBody.value = ''
     }
-  };
+  }
 
-  const likeMessage = messageId => {
-    var i = 0;
-    var j = 0;
-    var liked = false;
+  const likeMessage = (messageId) => {
+    var i = 0
+    var j = 0
+    var liked = false
     for (i = 0; i < msgObjs.length; i++) {
       if (msgObjs[i].id == messageId) {
         //
         //
         for (j = 0; j < msgObjs[i].likes.length; j++) {
           if (msgObjs[i].likes[j] === props.userObj.username) {
-            liked = true;
-            break;
+            liked = true
+            break
           }
         }
         //
@@ -117,9 +114,9 @@ const Home = props => {
             JSON.stringify({
               likeFlag: -1,
               id: messageId,
-              username: props.userObj.username
+              username: props.userObj.username,
             })
-          );
+          )
 
           // test: remove after
           // msgObjs[i].likes.splice(j, 1);
@@ -131,58 +128,67 @@ const Home = props => {
             JSON.stringify({
               likeFlag: 1,
               id: messageId,
-              username: props.userObj.username
+              username: props.userObj.username,
             })
-          );
+          )
 
           // test: remove after
           // msgObjs[i].likes.push(props.userObj.username);
           // setMsgObjs([...msgObjs]);
           // end
         }
-        break;
+        break
       }
     }
-  };
+  }
 
   return (
-    <div id="Home-root">
-      {!props.isUser || props.userObj == null ? <Redirect to="/login" /> : null}
+    <div
+      id='Home-root'
+      style={{ backgroundColor: theme.palette.common.background }}
+    >
+      {!props.isUser || props.userObj == null ? <Redirect to='/login' /> : null}
 
       <SwipeableDrawer
-        anchor={"right"}
+        anchor={'right'}
         open={drawerOpen}
         onClose={() => {
-          setDrawerOpen(false);
+          setDrawerOpen(false)
         }}
       >
         <CurrentActiveUsers setDrawerOpen={setDrawerOpen} />
       </SwipeableDrawer>
 
-      <div id="Home-header">
+      <div id='Home-header'>
         <Button
-          className="Home-header-button"
-          variant="contained"
-          color="primary"
-          size="large"
+          className='Home-header-button'
+          variant='contained'
+          color='primary'
+          size='large'
           onClick={() => props.logout()}
         >
           LOGOUT
         </Button>
 
+        <Button
+          onClick={props.handleThemeToggle}
+          style={{ marginLeft: 'auto' }}
+        >
+          <SettingsBrightnessIcon />
+        </Button>
         <IconButton
-          aria-label="users"
+          aria-label='users'
           onClick={() => setDrawerOpen(!drawerOpen)}
         >
-          <AccountCircleIcon color="primary" style={{ fontSize: 45 }} />
+          <AccountCircleIcon color='primary' style={{ fontSize: 45 }} />
         </IconButton>
       </div>
-      <div id="Home-body">
+      <div id='Home-body'>
         {msgObjs != null
           ? msgObjs.map((msgObj, i) => {
-              var self = false;
+              var self = false
               if (msgObj.username === props.userObj.username) {
-                self = true;
+                self = true
               }
               return (
                 <Message
@@ -193,48 +199,45 @@ const Home = props => {
                   likeCount={msgObj.likes.length}
                   likeAction={() => likeMessage(msgObj.id)}
                 />
-              );
+              )
             })
           : null}
       </div>
 
-      <div id="Home-footer">
+      <div id='Home-footer'>
         <TextField
-          id="message-input"
-          style={{ width: "80%" }}
-          label=""
-          defaultValue=""
-          variant="outlined"
+          id='message-input'
+          style={{ width: '80%' }}
+          label=''
+          defaultValue=''
+          variant='outlined'
         />
         <IconButton
-          aria-label="send"
+          aria-label='send'
           onClick={() => {
-            sendMessage();
+            sendMessage()
           }}
         >
-          <SendIcon fontSize="large" />
+          <SendIcon fontSize='large' />
         </IconButton>
       </div>
     </div>
-  );
-};
-
-const mapStateToProps = state => {
-  return {
-    isUser: state.isUser,
-    userObj: state.userObj
-    // userObj: { id: "248324", username: "jainamshah" }
-  };
-};
-function mapDispatchToProps(dispatch) {
-  return {
-    logout: () => dispatch(logout())
-  };
+  )
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Home);
+const mapStateToProps = (state) => {
+  return {
+    isUser: state.isUser,
+    userObj: state.userObj,
+    // userObj: { id: "248324", username: "jainamshah" }
+  }
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    logout: () => dispatch(logout()),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
 
 // STYLING
--- frontend/src/styles/theme.js
+++ frontend/src/styles/theme.js
@@ -2,49 +2,50 @@ import { createMuiTheme } from '@material-ui/core/styles'
 
 // A custom theme for this app
 export const darkTheme = createMuiTheme({
+  overrides: {
+    // needed for chrome yellow autofill
+    MuiInputBase: {
+      input: {
+        '&:-webkit-autofill': {
+          transitionDelay: '9999s',
+          transitionProperty: 'background-color, color',
+        },
+      },
+    },
+  },
   palette: {
+    common: {
+      black: '#1c181d',
+      eggshell: '#f1f1e1',
+      white: '#FFFFFF',
+      background: '#a682a6'
+    },
+    header: {
+      background: '#f5f5f5',
+      textColor: '#696969',
+    },
+    primary: {
+      main: '#fba23c',
+    },
+    secondary: {
+      main: '#00B1A5',
+    },
+    tertiary: {
+      main: '#A168FF',
+    },
     type: 'dark',
-    //   common: {
-    //     black: '#1c181d',
-    //     white: '#FFFFFF',
-    //     eggshell: '#f1f1e1'
-    //   },
-    //   primary: {
-    //     main: '#27AAE1'
-    //   },
-    //   header: {
-    //     background: '#696969',
-    //     textColor: '#f5f5f5'
-    //   },
-    //   secondary: {
-    //     main: '#f0544f',
-    //   },
-    //   tertiary: {
-    //     main: '#ffd166'
-    //   },
-    //   error: {
-    //     main: red.A400,
-    //   },
-    //   background: {
-    //     default: '#696969',
-    //   },
-    //   card: {
-    //     chipBackground: '#D3D3D3'
-    //   }
+    // error: {
+    //   main: red.A400,
     // },
-    // overrides: {
-    //   MuiPaper: {
-    //     root: {
-    //       backgroundColor: '#191b21'
-    //     }
-    //   },
-    //   MuiCard: {
-    //     root: {
-    //       backgroundColor: '#696969'
-    //     }
-    //   }
+    // background: {
+    //   default: '#f5f5f5',
+    // },
+    // card: {
+    //   chipBackground: '#D3D3D3',
+    // },
   },
 })
+
 
 export const lightTheme = createMuiTheme({
   overrides: {
@@ -63,6 +64,7 @@ export const lightTheme = createMuiTheme({
       black: '#1c181d',
       eggshell: '#f1f1e1',
       white: '#FFFFFF',
+      background: '#fde4fd'
     },
     header: {
       background: '#f5f5f5',
